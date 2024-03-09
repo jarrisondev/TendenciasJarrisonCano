e2270f30d65494c2ae0d18a6f3d8b8ca29346639
Jarrison Cano <jarrison.cano@hotmail.com>
2024-03-09 10:22:00 -0500

    add main models

--- veterinariaCLI/models/model.py
+++ veterinariaCLI/models/model.py
@@ -1,33 +1,210 @@
-class Person:
-    def __init__(self, id, name, age, role, username, password):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.role = role
-        self.username = username
-        self.password = password
-
-
-class Pet:
-    def __init__(self, id, name, age, ownerId, spice, race, features):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.ownerId = ownerId
-        self.spice = spice
-        self.race = race
-        self.features = features
-
-
-class Veterinary:
-    def __init__(self):
-        self.persons = []
-        self.pets = []
-        self.clinicalHistory = {}
-
+import datetime
+from typing import List
+from utils import validators
 
 Roles = {
-    "Admin": "Admin",
-    "Veterinarian": "Veterinarian",
-    "Client": "Client",
+    "administrative": 1,
+    "informationSupport": 2,
+    "nurse": 3,
+    "doctor": 4,
+    "humanResources": 5,
 }
+
+
+class Person:
+    def __init__(
+        self, id, name, role, username, password, email, phone, birthDate, address
+    ):
+        self.name: str = validators.stringValidator(name)
+        self.id: int = id
+        self.email: str = validators.emailValidator(email)
+        self.phone: int = validators.phoneValidator(phone)
+        self.birthDate: str = validators.birthvalidators.DateValidator(birthDate)
+        self.address: str = validators.addressValidator(address)
+        self.role: str = validators.roleValidator(role)
+        self.username: str = validators.usernameValidator(username)
+        self.password: str = validators.passwordValidator(password)
+
+
+class Patient:
+    def __init__(
+        self,
+        id,
+        name,
+        birthDate,
+        gender,
+        address,
+        phone,
+        email,
+        # emergency Contact,
+        emergencyContactName,
+        emergencyContactPhone,
+        emergencyContactRelationship,
+        # medacal Insurance,
+        medicalInsuranceName,
+        medicalInsuranceNumber,
+        medicalInsuranceStatus,
+        medicalInsuranceExpirationDate,
+    ):
+        self.id: int = id
+        self.name: str = validators.stringValidator(name)
+        self.birthDate: str = validators.birthvalidators.DateValidator(birthDate)
+        self.address: str = validators.addressValidator(address)
+        self.phone: int = validators.phoneValidator(phone)
+        self.email: str = validators.emailValidator(email)
+        self.emergencyContactName: str = validators.stringValidator(
+            emergencyContactName
+        )
+        self.emergencyContactPhone: int = validators.phoneValidator(
+            emergencyContactPhone
+        )
+        self.emergencyContactRelationship: str = validators.stringValidator(
+            emergencyContactRelationship
+        )
+        self.medicalInsuranceName: str = validators.stringValidator(
+            medicalInsuranceName
+        )
+        self.medicalInsuranceNumber: int = validators.integerValidator(
+            medicalInsuranceNumber
+        )
+        self.medicalInsuranceStatus: bool = validators.booleanValidator(
+            medicalInsuranceStatus
+        )
+        self.medicalInsuranceExpirationDate: str = validators.dateValidator(
+            medicalInsuranceExpirationDate
+        )
+
+    def getAge(self):
+        return datetime.date.today().year - int(self.birthDate[6:])
+
+
+class HumanResources(Person):
+    def __init__(
+        self,
+        id,
+        name,
+        username,
+        password,
+        email,
+        phone,
+        birthDate,
+        address,
+    ):
+        super().__init__(
+            id,
+            name,
+            Roles["humanResources"],
+            username,
+            password,
+            email,
+            phone,
+            birthDate,
+            address,
+        )
+
+    def createPerson(
+        id, name, role, username, password, email, phone, birthDate, address
+    ):
+        return Person(
+            id, name, role, username, password, email, phone, birthDate, address
+        )
+
+
+class Administrative(Person):
+    def __init__(
+        self,
+        id,
+        name,
+        username,
+        password,
+        email,
+        phone,
+        birthDate,
+        address,
+    ):
+        super().__init__(
+            id,
+            name,
+            Roles["administrative"],
+            username,
+            password,
+            email,
+            phone,
+            birthDate,
+            address,
+        )
+
+    def createPatient(
+        id,
+        name,
+        birthDate,
+        gender,
+        address,
+        phone,
+        email,
+        emergencyContactName,
+        emergencyContactPhone,
+        emergencyContactRelationship,
+    ):
+        return Patient(
+            id,
+            name,
+            birthDate,
+            gender,
+            address,
+            phone,
+            email,
+            emergencyContactName,
+            emergencyContactPhone,
+            emergencyContactRelationship,
+        )
+
+
+class InformationSupport(Person):
+    def __init__(
+        self,
+        id,
+        name,
+        username,
+        password,
+        email,
+        phone,
+        birthDate,
+        address,
+    ):
+        super().__init__(
+            id,
+            name,
+            Roles["informationSupport"],
+            username,
+            password,
+            email,
+            phone,
+            birthDate,
+            address,
+        )
+
+
+class Medicine:
+    pass
+
+
+class Hospital:
+    def __init__(self):
+        self.patients: List[Patient] = []
+        self.humanResources: List[HumanResources] = []
+        self.administratives: List[Administrative] = []
+        self.informationSupports: List[InformationSupport] = []
+        self.nurses = []
+        self.doctors = []
+        self.medicines: List[Medicine] = []
+
+    def getEmployees(self):
+        employees: Person = []
+        employees.append(self.administratives)
+        employees.append(self.humanResources)
+        employees.append(self.informationSupports)
+        employees.append(self.nurses)
+        employees.append(self.doctors)
+
+        return employees
--- veterinariaCLI/utils/validators.py
+++ veterinariaCLI/utils/validators.py
@@ -1,11 +1,99 @@
-def stringValidator(value):
-    if value == "" or value == None:
+import datetime
+from models import Roles
+
+
+def stringValidator(value: str):
+    if value != "" and value != None:
+        return value
+    else:
         raise Exception("El valor ingresado no es una cadena de texto")
 
 
-def integerValidator(value):
-    stringValidator(value)
-    try:
-        return int(value)
-    except:
-        raise Exception("No es un numero valido")
+def integerValidator(value: int):
+    if value.isdigit():
+        return value
+    else:
+        raise Exception("El valor ingresado no es un numero")
+
+
+def booleanValidator(value: bool):
+    if value == True or value == False:
+        return value
+    else:
+        raise Exception("El valor ingresado no es un booleano")
+
+
+def dateValidator(date: str):
+    if (
+        len(date) == 10
+        and date[2] == "/"
+        and date[5] == "/"
+        and int(date[0:2]) < 32
+        and int(date[3:5]) < 13
+        and date[6:].isdigit()
+        and len(date[6:]) == 4
+    ):
+        return date
+    else:
+        raise Exception("La fecha no es valida")
+
+
+def birthDateValidator(birthDate: str):
+    today = datetime.date.today()
+    if dateValidator(birthDate) and today.year - int(birthDate[6:]) <= 150:
+        return birthDate
+    else:
+        raise Exception("La fecha de nacimiento no es valida")
+
+
+def phoneValidator(phone: int):
+    if len(phone) < 10 and len(phone) > 1 and phone.isdigit():
+        return phone
+    else:
+        raise Exception("El numero de telefono no es valido")
+
+
+def emailValidator(email: str):
+    if "@" in email and "." in email:
+        return email
+    else:
+        raise Exception("El correo no es valido")
+
+
+def usernameValidator(username: str):
+    if len(username) > 0 and len(username) <= 15:
+        return username
+    else:
+        raise Exception("El username no es valido")
+
+
+def passwordValidator(password: str):
+    if (
+        len(password) > 7
+        and any(char.isdigit() for char in password)
+        and any(char.isupper() for char in password)
+    ):
+        return password
+    else:
+        raise Exception("La contraseña no es valida")
+
+
+def genderValidator(gender: str):
+    if gender == "M" or gender == "F":
+        return gender
+    else:
+        raise Exception("El genero no es valido")
+
+
+def addressValidator(address: str):
+    if len(address) <= 30:
+        return address
+    else:
+        raise Exception("La direccion es muy larga")
+
+
+def roleValidator(role: str):
+    if role in Roles:
+        return role
+    else:
+        raise Exception("El rol no es valido")
