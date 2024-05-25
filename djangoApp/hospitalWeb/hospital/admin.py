77bc11fad2838ea35b1568f57fde8c7c8a65a7e6
Jarrison Cano <me@jarrison.dev>
2024-05-25 17:26:15 -0500

    add models to admin.site

--- djangoApp/hospitalWeb/hospital/admin.py
+++ djangoApp/hospitalWeb/hospital/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from hospital.models import (
+    Employee,
+    Patient,
+    Medicine,
+    Order,
+    MedicineOrder,
+    ProcedureOrder,
+    DiagnosticHelpOrder,
+)
 
 # Register your models here.
+
+
+admin.site.register(Employee)
+admin.site.register(Patient)
+admin.site.register(Medicine)
+admin.site.register(Order)
+admin.site.register(MedicineOrder)
+admin.site.register(ProcedureOrder)
+admin.site.register(DiagnosticHelpOrder)
