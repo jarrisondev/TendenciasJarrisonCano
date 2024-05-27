e7c15582c889b5cf4f24b184473983dc4cdfd756
Jarrison Cano <me@jarrison.dev>
2024-05-27 04:38:53 -0500

    feat: add zustand

--- client/src/features/dashboard/dashboard.tsx
+++ client/src/features/dashboard/dashboard.tsx
@@ -8,6 +8,8 @@ export const Dashboard = () => {
       <div className="border-b">
         <div className="flex h-16 items-center px-4">
           <nav className={'flex items-center mx-6 space-x-4 lg:space-x-6'}>
+            <h2 className="text-3xl font-bold tracking-tight">Hospital</h2>
+
             <Link to="/examples/dashboard" className="text-sm font-medium transition-colors hover:text-primary">
               Pacientes
             </Link>
@@ -36,8 +38,7 @@ export const Dashboard = () => {
         </div>
       </div>
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Hospital</h2>
+        <div className="flex items-center justify-end space-y-2">
           <div className="flex items-center space-x-2">
             <Button>Download</Button>
           </div>
