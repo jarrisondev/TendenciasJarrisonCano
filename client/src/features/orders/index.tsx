0afd273768a3af1a23deebe27fa62d97774c5d74
Jarrison Cano <me@jarrison.dev>
2024-06-01 09:23:12 -0500

    chore: Add exceljs dependency for exporting orders to Excel

--- client/src/features/orders/index.tsx
+++ client/src/features/orders/index.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@/components/ui/button'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { useState } from 'react'
-import { Plus } from 'lucide-react'
+import { FileDown, Plus } from 'lucide-react'
 import { Order } from './order'
 import { IOrder, useDeleteOrder, useFetchOrders } from './services'
 import { useFetchPatients } from '../patients/services'
 import { useFetchEmployees } from '../employees/services'
+import Exceljs from 'exceljs'
+import { useFetchMedicines } from '../medicines/services'
 
 export const Orders = () => {
   const fetchList = useFetchOrders()
@@ -14,7 +16,51 @@ export const Orders = () => {
   const fetchEmployee = useFetchEmployees()
   const [openOrder, setOpenOrder] = useState<IOrder | null>(null)
 
+  const medicines = useFetchMedicines()
+
   const orders = fetchList.data ?? []
+
+  const handleExportExcel = (order: IOrder) => {
+    const workbook = new Exceljs.Workbook()
+    const worksheet = workbook.addWorksheet('Orden')
+
+    worksheet.getColumn('A').width = 30
+    worksheet.addRow(['Doctor:', fetchEmployee.data?.find((employee) => employee.id === order.doctor)?.name])
+    worksheet.addRow(['Paciente:', fetchPatient.data?.find((patient) => patient.id === order.patient)?.name])
+    worksheet.addRow(['Fecha de creación:', new Date(order.createdAt).toLocaleDateString()])
+    worksheet.addRow([' '])
+
+    worksheet.addRow(['Medicamentos']).font = { bold: true }
+    worksheet.addRow(['Nombre', 'Precio', 'Cantidad']).font = { bold: true }
+    order.medicineOrder.forEach((item) => {
+      const medicine = medicines.data?.find((medicine) => medicine.id === item.medicine)
+      worksheet.addRow([medicine?.name, medicine?.price, item.quantity])
+    })
+    worksheet.addRow([' '])
+
+    worksheet.addRow(['Procedimientos']).font = { bold: true }
+    worksheet.addRow(['Nombre', 'Precio', 'Cantidad', 'Requiere asistencia']).font = { bold: true }
+    order.procedureOrder.forEach((item) => {
+      worksheet.addRow([item.name, item.price, item.quantity, item.requireAssistance ? 'Si' : 'No'])
+    })
+    worksheet.addRow([' '])
+
+    worksheet.addRow(['Ayudas diagnosticas']).font = { bold: true }
+    worksheet.addRow(['Nombre', 'Precio', 'Cantidad', 'Requiere asistencia']).font = { bold: true }
+    order.diagnosticHelpOrder.forEach((item) => {
+      worksheet.addRow([item.name, item.price, item.quantity, item.requireAssistance ? 'Si' : 'No'])
+    })
+
+    workbook.xlsx.writeBuffer().then((buffer) => {
+      const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' })
+      const url = URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.href = url
+      a.download = 'orden.xlsx'
+      a.click()
+      URL.revokeObjectURL(url)
+    })
+  }
 
   return (
     <div>
@@ -67,10 +113,21 @@ export const Orders = () => {
                   <TableCell>{item.procedureOrder.length}</TableCell>
                   <TableCell>{item.diagnosticHelpOrder.length}</TableCell>
 
-                  <TableCell className="text-right">
-                    <Button onClick={() => setOpenOrder(item)}>Editar</Button>
+                  <TableCell className="flex justify-end">
                     <Button
-                      className="ml-2 bg-red-500 hover:bg-red-600"
+                      size="icon"
+                      variant="secondary"
+                      onClick={() => {
+                        handleExportExcel(item)
+                      }}
+                    >
+                      <FileDown size={16} />
+                    </Button>
+                    <Button className="ml-2" onClick={() => setOpenOrder(item)}>
+                      Editar
+                    </Button>
+                    <Button
+                      className="ml-2"
                       onClick={() => {
                         if (item?.id) {
                           deleteItem.mutateAsync(item?.id).then(() => {
@@ -78,7 +135,7 @@ export const Orders = () => {
                           })
                         }
                       }}
-                      variant={'secondary'}
+                      variant={'destructive'}
                     >
                       Eliminar
                     </Button>
